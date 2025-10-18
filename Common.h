f8e0488b6c9ab7d00442f155a0d5eb28f71db2d5
A先生 <142523398+axs66@users.noreply.github.com>
2025-10-18 15:13:21 +0800

    Update Common.h

--- Common.h
+++ Common.h
@@ -1,36 +1,48 @@
 #define UNRESTRICTED_AVAILABILITY
-#import <UIKit/UIKit.h>
-#import <AVFoundation/AVFoundation.h>
+#import <PSHeader/CameraApp/CameraApp.h>
+#import <PSHeader/CameraMacros.h>
 
-// 私有类声明 - 这些类在运行时通过 CameraUI 框架动态链接
+@interface CAMElapsedTimeView (Addition)
+- (void)pauseTimer;
+- (void)resumeTimer;
+- (void)updateUI:(BOOL)pause recording:(BOOL)recording;
+@end
 
-// 视频配置模式枚举
-typedef NS_ENUM(NSInteger, VideoConfigurationMode) {
-    VideoConfigurationModeDefault = 0,
-    VideoConfigurationMode1080p60 = 1,
-    VideoConfigurationMode720p120 = 2,
-    VideoConfigurationMode720p240 = 3,
-    VideoConfigurationMode1080p120 = 4,
-    VideoConfigurationMode4k30 = 5,
-    VideoConfigurationMode720p30 = 6,
-    VideoConfigurationMode1080p30 = 7,
-    VideoConfigurationMode1080p240 = 8,
-    VideoConfigurationMode4k60 = 9,
-    VideoConfigurationMode4k24 = 10,
-    VideoConfigurationMode1080p25 = 11,
-    VideoConfigurationMode4k25 = 12,
-    VideoConfigurationMode4k120 = 13,
-    VideoConfigurationMode4k100 = 14,
-    VideoConfigurationModeCount
-};
+@interface AVCaptureMovieFileOutput (Private)
+- (BOOL)isRecordingPaused;
+- (void)pauseRecording;
+- (void)resumeRecording;
+@end
 
-// 全局变量
-extern NSInteger devices[];
-extern NSInteger toFPS[];
-extern NSString *NSTimerPauseDate;
-extern NSString *NSTimerPreviousFireDate;
+@interface CAMLiquidShutterRenderer : NSObject
+- (void)renderIfNecessary;
+@end
 
-// 函数声明
-NSString *title(VideoConfigurationMode mode);
+@interface UIView (Private)
+@property (nonatomic, assign, setter=_setShouldReverseLayoutDirection:) BOOL _shouldReverseLayoutDirection;
+@end
 
-// 接口扩展 - 这些类别定义已移动到 Tweak.x 文件中
+extern CGRect UIRectIntegralWithScale(CGRect rect, CGFloat scale);
+extern CGFloat UIRoundToViewScale(CGFloat value, UIView *view);
+
+@interface CAMViewfinderViewController (Addition)
+@property (retain, nonatomic) UILongPressGestureRecognizer *rpGesture;
+@property (nonatomic, retain) CUShutterButton *_pauseResumeDuringVideoButton;
+- (void)_createPauseResumeDuringVideoButtonIfNecessary;
+- (void)_embedPauseResumeDuringVideoButtonWithLayoutStyle:(NSInteger)layoutStyle;
+- (void)_updatePauseResumeDuringVideoButton:(BOOL)paused;
+@end
+
+@interface CAMDynamicShutterControl (Addition)
+@property (nonatomic, retain) CUShutterButton *pauseResumeDuringVideoButton;
+@property (nonatomic, assign) BOOL overrideShutterButtonColor;
+@end
+
+@interface CAMBottomBar (Addition)
+@property (nonatomic, retain) CUShutterButton *pauseResumeDuringVideoButton;
+- (void)_layoutPauseResumeDuringVideoButtonForLayoutStyle:(NSInteger)layoutStyle;
+- (void)_layoutPauseResumeDuringVideoButtonForTraitCollection:(UITraitCollection *)traitCollection;
+@end
+
+NSString *NSTimerPauseDate = @"NSTimerPauseDate";
+NSString *NSTimerPreviousFireDate = @"NSTimerPreviousFireDate";
